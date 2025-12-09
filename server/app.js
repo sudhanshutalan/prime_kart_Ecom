145f50546c6c9e8d2638c6708b4e6e4fb1789b79
Sudhanshu Talan <sudhanshutalan@gmail.com>
2025-12-09 22:34:46 +0530

    feat: initialize server package with essential dependencies and script

---
+++ server/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+
+const app = express();
+
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    tempFileDir: "./uploads",
+    useTempFiles: true,
+  })
+);
+
+export default app;
