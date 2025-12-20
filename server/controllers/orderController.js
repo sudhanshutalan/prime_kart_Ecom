35f603c62d3013b20e26c7550ae98608a9c5402b
Sudhanshu Talan <sudhanshutalan@gmail.com>
2025-12-20 22:43:42 +0530

    refactor: enhance order placement validation and error handling

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -15,65 +15,119 @@ export const placeNewOrder = asyncHandler(async (req, res) => {
     phone,
     orderedItems,
   } = req.body;
-});
 
-if (
-  !full_name ||
-  !state ||
-  !city ||
-  !country ||
-  !address ||
-  !pincode ||
-  !phone
-) {
-  throw new ApiError(400, "all fields are required");
-}
-
-const items = Array.isArray(orderedItems)
-  ? orderedItems
-  : JSON.parse(orderedItems);
-
-const productIds = items.map((item) => item.product.id);
-const { rows: products } = await database.query(
-  `SELECT id,price,stock name FROM products WHERE id = ANY($1::uuid[])`,
-  [productIds]
-);
-
-let totalPrice = 0;
-const values = [];
-const placeholder = [];
-
-items.forEach((item, index) => {
-  const product = products.find((p) => p.id === item.product.id);
-
-  if (!product) {
-    throw new ApiError(400, "product not found");
+  if (
+    !full_name ||
+    !state ||
+    !city ||
+    !country ||
+    !address ||
+    !pincode ||
+    !phone
+  ) {
+    throw new ApiError(400, "Please provide complete shipping details.");
   }
 
-  if (item.quantity > product.stock) {
-    throw new ApiError(
-      400,
-      `Only${product.stock} units are available for ${product.name}`
+  const items = Array.isArray(orderedItems)
+    ? orderedItems
+    : JSON.parse(orderedItems);
+
+  if (!items || items.length === 0) {
+    return next(new ErrorHandler("No items in cart.", 400));
+  }
+  const productIds = items.map((item) => item.product.id);
+  const { rows: products } = await database.query(
+    `SELECT id, price, stock, name FROM products WHERE id = ANY($1::uuid[])`,
+    [productIds]
+  );
+
+  let total_price = 0;
+  const values = [];
+  const placeholders = [];
+
+  items.forEach((item, index) => {
+    const product = products.find((p) => p.id === item.product.id);
+
+    if (!product) {
+      return next(
+        new ErrorHandler(`Product not found for ID: ${item.product.id}`, 404)
+      );
+    }
+
+    if (item.quantity > product.stock) {
+      throw new ApiError(
+        400,
+        `Only ${product.stock} units available for ${product.name}`
+      );
+    }
+
+    const itemTotal = product.price * item.quantity;
+    total_price += itemTotal;
+
+    values.push(
+      null,
+      product.id,
+      item.quantity,
+      product.price,
+      item.product.images[0].url || "",
+      product.name
     );
+
+    const offset = index * 6;
+
+    placeholders.push(
+      `($${offset + 1}, $${offset + 2}, $${offset + 3}, $${offset + 4}, $${
+        offset + 5
+      }, $${offset + 6})`
+    );
+  });
+
+  const tax_price = 0.18;
+  const shipping_price = total_price >= 50 ? 0 : 2;
+  total_price = Math.round(
+    total_price + total_price * tax_price + shipping_price
+  );
+
+  const orderResult = await database.query(
+    `INSERT INTO orders (buyer_id, total_price, tax_price, shipping_price) VALUES ($1, $2, $3, $4) RETURNING *`,
+    [req.user.id, total_price, tax_price, shipping_price]
+  );
+
+  const orderId = orderResult.rows[0].id;
+
+  for (let i = 0; i < values.length; i += 6) {
+    values[i] = orderId;
   }
 
-  const itemTotal = product.price * item.quantity;
-  total_price += itemTotal;
-
-  values.push(
-    null,
-    product.id,
-    item.quantity,
-    product.price,
-    item.product.images[0].url || "",
-    product.name
+  await database.query(
+    `
+    INSERT INTO order_items (order_id, product_id, quantity, price, image, title)
+    VALUES ${placeholders.join(", ")} RETURNING *
+    `,
+    values
   );
 
-  const offset = index * 6;
-
-  placeholders.push(
-    `($${offset + 1}, $${offset + 2}, $${offset + 3}, $${offset + 4}, $${
-      offset + 5
-    }, $${offset + 6})`
+  await database.query(
+    `
+    INSERT INTO shipping_info (order_id, full_name, state, city, country, address, pincode, phone)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *
+    `,
+    [orderId, full_name, state, city, country, address, pincode, phone]
   );
+
+  const paymentResponse = await generatePaymentIntent(orderId, total_price);
+
+  if (!paymentResponse.success) {
+    throw new ApiError(500, "Payment failed. Try again.");
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { paymentIntent: paymentResponse.client_secret, total_price },
+        "Order placed successfully. Please proceed to payment."
+      )
+    );
 });
